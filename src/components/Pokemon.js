6d7dba65909ef4853cc3d4adbaa49bb523e20b75
MateusLiraFront <mlgtifront@gmail.com>
2025-09-12 08:27:50 -0300

    first commit

---
+++ src/components/Pokemon.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Pokemon = () => {
+
+    return(
+        <div>
+            <h1>
+                Pokemon
+            </h1>
+        </div>
+    )
+};
+
+export default Pokemon;
