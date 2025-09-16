5f8fdaa55b7b358ab784082688a773cda6bb3d98
MateusLiraFront <mlgtifront@gmail.com>
2025-09-16 12:45:49 -0300

    exibição dos pokemons

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -7,6 +7,21 @@ const Pokemon = (props) => {
             <div className="pokemon-image-container">
                 <img src={pokemon.sprites.front_default} alt={pokemon.name} />
             </div>
+            <div className="card-body">
+                <div clalssName="card-top">
+                    <h3>{pokemon.name}</h3>
+                    <div>#{pokemon.id}</div>
+                </div>
+                <div clalssName="card-bot">
+                    <div className="pokemon-type">
+                        {pokemon.types.map((type, index) => {
+                        return (
+                            <div key={index} className="pokemon-type-text">{type.type.name}</div>
+                        )
+                    })}
+                    </div>
+                </div>
+            </div>
             {pokemon.name}
             {/* <h1> Pokemon </h1> */}
         </div>
