5f95e529832ad26ea01eaf68ee49234eb8a57be8
MateusLiraFront <mlgtifront@gmail.com>
2025-09-16 11:58:21 -0300

    exibição dos pokemons

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -3,7 +3,10 @@ import React from "react";
 const Pokemon = (props) => {
     const {pokemon} = props
     return(
-        <div>
+        <div className="pokemon-card">
+            <div className="pokemon-image-container">
+                <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+            </div>
             {pokemon.name}
             {/* <h1> Pokemon </h1> */}
         </div>
