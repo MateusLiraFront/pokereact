daf7541cecd72b9ee79afea4cd3325ca67c40b93
MateusLiraFront <mlgtifront@gmail.com>
2025-09-16 09:11:02 -0300

    search pokemon

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-
+import React from 'react';
+import { searchPokemon } from './api';
 import './App.css';
 import NavBar from './components/NavBar';
 import SearchBar from './components/Searchbar';
 
+
 function App() {
+
+    // const onSearchHandler = async (pokemon) =>{
+    //   const result = await searchPokemon(pokemon)
+    //   console.log("Pokemon:", result)
+    // }
   return (
     <div>
       <NavBar/>
-      <SearchBar/>
+      <SearchBar
+        // onSearch={onSearchHandler}
+      />
       <div className="App">
       
     </div>
--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,25 +1,64 @@
 import React, {useState} from "react";
-
+import { searchPokemon } from "../api";
 
 const SearchBar = () => {
     const [search, setSearch] = useState("Metacross")
-
+    const [pokemon, setPokemon] = useState()
+   
     const onChangeHandler = (e) => {
         console.log("pokemon", e.target.value)
         setSearch(e.target.value)
     }
-
-    const onClickHandler = () => {
-        console.log("Pokemon: ", search)
+    
+    const onButtonClickHandler = () => {
+        onSearchHandler(search)
     }
+
+    const onSearchHandler = async (pokemon) => {
+        const result = await searchPokemon(pokemon)
+        setPokemon(result)
+    }
+    // const onClickHandler = () => {
+    //     onSearch(search)
+    // }
     return(
+       
         <div className="searchbar-container">
-            <div className="searchbar">
-                <input placeholder="Buscar Pokemon" onChange={onChangeHandler}></input>
-                <h1>Pokemon: </h1>{search}
-            </div>
-            <div className="search-btn">
-                <button onClick={onClickHandler}> Buscar </button>
+            <div className="column">
+                <div className="searchbar campo row">
+                    <div className="searchbar">
+                        <input placeholder="Buscar Pokemon" onChange={onChangeHandler}></input>
+                        <h1>Pokemon: </h1>
+                    </div>
+                    <div className="search-btn">
+                        <button onClick={onButtonClickHandler}> Buscar </button>
+                    </div>
+                </div>
+           
+           
+            {pokemon ? (
+                <div>
+                    <div> <strong> Nome: </strong> {pokemon.name}</div>
+                    <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+                    <div> <strong> Peso: </strong>{pokemon.weight}</div>
+                    <div> <strong>Tipos:</strong>
+                        <ul>
+                            {pokemon.types.map((typeObj, index) => (
+                                <li key={index}>{typeObj.type.name}</li>
+                            ))}
+                        </ul></div>
+                    <div><strong>Status:</strong>
+                        <ul>
+                            {pokemon.stats.map((statObj, index) => (
+                                <li key={index}>
+                                    {statObj.stat.name}: {statObj.base_stat}
+                                </li>
+                            ))}
+                        </ul></div>
+                    
+                </div>
+            ) : null}
+           
             </div>
         </div>
     )
